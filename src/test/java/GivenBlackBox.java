af64cd6aa37161188ee97cb292202c4c2dc684a8
sladam15 <Brandon@Brandon-PC>
2021-04-02 05:50:03 -0700

    Update Blackbox.java

--- src/test/java/GivenBlackBox.java
+++ src/test/java/GivenBlackBox.java
@@ -8,8 +8,7 @@ import org.junit.runners.Parameterized.Parameters;
 import java.lang.reflect.Constructor;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+
 
 //import main.java.BearWorkshop;
 
@@ -67,7 +66,7 @@ public class GivenBlackBox {
         
         BattleScenario fight1 = createBattleScenario(attacker1, defender1); 
         System.out.println("    BvsRSunnyGround");
-        
+
         fight1.setEnvironment(Environment.Weather.sunny);
         Attack attack = new Attack(80, "Ground");
         
@@ -85,19 +84,206 @@ public class GivenBlackBox {
      */
     @Test
     public void RvsBSunnyGround() throws Exception {
-        
+
         Mascotmon attacker1 = new Mascotmon(Mascotmon.Name.RALPHIE);
         Mascotmon defender1 = new Mascotmon(Mascotmon.Name.BULLY);
-        
-        BattleScenario fight1 = createBattleScenario(attacker1, defender1); 
+
+        BattleScenario fight1 = createBattleScenario(attacker1, defender1);
         System.out.println("    RvsBSunnyGround");
-        
+
         fight1.setEnvironment(Environment.Weather.sunny);
         Attack attack = new Attack(80, "Ground");
-        
+
         //Calculation: 80 * 1.2 * 1 * 1 - 40 * 1 *1
         // 80 put into attack manually, Ralhpie gets bonus for ground attack no weather bonuses or type bonus
-        
+
+        double damage = fight1.calculateDamage(attack, attacker1, defender1);
+        System.out.println("         Damage dealt: " + damage);
+        assertEquals(damage, 56, 0.2);
+    }
+
+    @Test
+    public void AvsRDroughtWater() throws Exception {
+
+        Mascotmon attacker1 = new Mascotmon(Mascotmon.Name.ALBERT);
+        Mascotmon defender1 = new Mascotmon(Mascotmon.Name.RALPHIE);
+
+        BattleScenario fight1 = createBattleScenario(attacker1, defender1);
+        System.out.println("    AvsRDroughtWater");
+
+        fight1.setEnvironment(Environment.Weather.drought);
+        Attack attack = new Attack(80, "Water");
+
+        double damage = fight1.calculateDamage(attack, attacker1, defender1);
+        System.out.println("         Damage dealt: " + damage);
+        assertEquals(damage, 1, 0.2);
+    }
+
+    @Test
+    public void SvsRRainyFire() throws Exception {
+
+        Mascotmon attacker1 = new Mascotmon(Mascotmon.Name.SPARKY);
+        Mascotmon defender1 = new Mascotmon(Mascotmon.Name.RALPHIE);
+
+        BattleScenario fight1 = createBattleScenario(attacker1, defender1);
+        System.out.println("    SvsRRainyFire");
+
+        fight1.setEnvironment(Environment.Weather.rainy);
+        Attack attack = new Attack(70, "Fire");
+
+        double damage = fight1.calculateDamage(attack, attacker1, defender1);
+        System.out.println("         Damage dealt: " + damage);
+        assertEquals(damage, 42, 0.2);
+    }
+
+    @Test
+    public void SvsBDefaultNone() throws Exception {
+
+        Mascotmon attacker1 = new Mascotmon(Mascotmon.Name.SPARKY);
+        Mascotmon defender1 = new Mascotmon(Mascotmon.Name.RALPHIE);
+
+        BattleScenario fight1 = createBattleScenario(attacker1, defender1);
+        System.out.println("    SvsBDefaultNone");
+
+        fight1.setEnvironment(Environment.Weather.rainy);
+        Attack attack = new Attack(0, "None");
+
+        double damage = fight1.calculateDamage(attack, attacker1, defender1);
+        System.out.println("         Damage dealt: " + damage);
+        assertEquals(damage, 0, 0.2);
+    }
+
+    @Test
+    public void SvsASunnyNormal() throws Exception {
+
+        Mascotmon attacker1 = new Mascotmon(Mascotmon.Name.SPARKY);
+        Mascotmon defender1 = new Mascotmon(Mascotmon.Name.ALBERT);
+
+        BattleScenario fight1 = createBattleScenario(attacker1, defender1);
+        System.out.println("    SvsASunnyNormal");
+
+        fight1.setEnvironment(Environment.Weather.sunny);
+        Attack attack = new Attack(80, "Normal");
+
+        double damage = fight1.calculateDamage(attack, attacker1, defender1);
+        System.out.println("         Damage dealt: " + damage);
+        assertEquals(damage, 25, 0.2);
+    }
+
+    @Test
+    public void AvsBDefaultWater() throws Exception {
+
+        Mascotmon attacker1 = new Mascotmon(Mascotmon.Name.ALBERT);
+        Mascotmon defender1 = new Mascotmon(Mascotmon.Name.BULLY);
+
+        BattleScenario fight1 = createBattleScenario(attacker1, defender1);
+        System.out.println("    AvsBDefaultWater");
+
+        fight1.setEnvironment(Environment.Weather.rainy);
+        Attack attack = new Attack(100, "Water");
+
+        double damage = fight1.calculateDamage(attack, attacker1, defender1);
+        System.out.println("         Damage dealt: " + damage);
+        assertEquals(damage, 110, 0.2);
+    }
+
+    @Test
+    public void BvsADroughtNormal() throws Exception {
+
+        Mascotmon attacker1 = new Mascotmon(Mascotmon.Name.BULLY);
+        Mascotmon defender1 = new Mascotmon(Mascotmon.Name.ALBERT);
+
+        BattleScenario fight1 = createBattleScenario(attacker1, defender1);
+        System.out.println("    BvsADroughtNormal");
+
+        fight1.setEnvironment(Environment.Weather.drought);
+        Attack attack = new Attack(90, "Normal");
+
+        double damage = fight1.calculateDamage(attack, attacker1, defender1);
+        System.out.println("         Damage dealt: " + damage);
+        assertEquals(damage, 51, 0.2);
+    }
+
+    @Test
+    public void AvsBDroughtNormal() throws Exception {
+
+        Mascotmon attacker1 = new Mascotmon(Mascotmon.Name.ALBERT);
+        Mascotmon defender1 = new Mascotmon(Mascotmon.Name.BULLY);
+
+        BattleScenario fight1 = createBattleScenario(attacker1, defender1);
+        System.out.println("    AvsBDroughtNormal");
+
+        fight1.setEnvironment(Environment.Weather.drought);
+        Attack attack = new Attack(90, "Normal");
+
+        double damage = fight1.calculateDamage(attack, attacker1, defender1);
+        System.out.println("         Damage dealt: " + damage);
+        assertEquals(damage, 50, 0.2);
+    }
+
+    @Test
+    public void SvsBRainyNone() throws Exception {
+
+        Mascotmon attacker1 = new Mascotmon(Mascotmon.Name.SPARKY);
+        Mascotmon defender1 = new Mascotmon(Mascotmon.Name.BULLY);
+
+        BattleScenario fight1 = createBattleScenario(attacker1, defender1);
+        System.out.println("    SvsBRainyNone");
+
+        fight1.setEnvironment(Environment.Weather.drought);
+        Attack attack = new Attack(0, "Normal");
+
+        double damage = fight1.calculateDamage(attack, attacker1, defender1);
+        System.out.println("         Damage dealt: " + damage);
+        assertEquals(damage, 0, 0.2);
+    }
+
+    @Test
+    public void BvsSRainyNone() throws Exception {
+
+        Mascotmon attacker1 = new Mascotmon(Mascotmon.Name.BULLY);
+        Mascotmon defender1 = new Mascotmon(Mascotmon.Name.SPARKY);
+
+        BattleScenario fight1 = createBattleScenario(attacker1, defender1);
+        System.out.println("    BvsSRainyNone");
+
+        fight1.setEnvironment(Environment.Weather.rainy);
+        Attack attack = new Attack(0, "None");
+
+        double damage = fight1.calculateDamage(attack, attacker1, defender1);
+        System.out.println("         Damage dealt: " + damage);
+        assertEquals(damage, 0, 0.2);
+    }
+
+    @Test
+    public void RvsASunnyFire() throws Exception {
+
+        Mascotmon attacker1 = new Mascotmon(Mascotmon.Name.BULLY);
+        Mascotmon defender1 = new Mascotmon(Mascotmon.Name.SPARKY);
+
+        BattleScenario fight1 = createBattleScenario(attacker1, defender1);
+        System.out.println("    RvsASunnyFire");
+
+        fight1.setEnvironment(Environment.Weather.sunny);
+        Attack attack = new Attack(100, "Fire");
+
+        double damage = fight1.calculateDamage(attack, attacker1, defender1);
+        System.out.println("         Damage dealt: " + damage);
+        assertEquals(damage, 50, 0.2);
+    }
+
+    @Test
+    public void RvsBDefaultGround() throws Exception {
+
+        Mascotmon attacker1 = new Mascotmon(Mascotmon.Name.RALPHIE);
+        Mascotmon defender1 = new Mascotmon(Mascotmon.Name.BULLY);
+
+        BattleScenario fight1 = createBattleScenario(attacker1, defender1);
+        System.out.println("    RvsBDefaultGround");
+
+        fight1.setEnvironment(Environment.Weather.neutral);
+        Attack attack = new Attack(80, "Ground");
+
         double damage = fight1.calculateDamage(attack, attacker1, defender1);
         System.out.println("         Damage dealt: " + damage);
         assertEquals(damage, 56, 0.2);
