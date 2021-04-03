89e251c11cef372d1587846ee4c36b88860446a1
sladam15 <Brandon@Brandon-PC>
2021-04-02 18:36:26 -0700

    Update syntax errors

--- src/test/java/GivenWhiteBox.java
+++ src/test/java/GivenWhiteBox.java
@@ -31,45 +31,5 @@ public class GivenWhiteBox {
         assertEquals(mon, attacker1);
     }
 
-    // Node coverage test case will visit every node in path
-    // 1-15 nodes are all executed in BattleScenario.java
-    // Both node coverage and edge coverage are similar here because
-    // there's parts of the code that repeat because of a monster
-    // fainting or not
-    @Test
-    public void testNodeCoverage() {
-        Mascotmon attacker1 = new Mascotmon(Mascotmon.Name.SPARKY);
-        Mascotmon defender1 = new Mascotmon(Mascotmon.Name.RALPHIE);
-
-        BattleScenario fight1 = new BattleScenario(attacker1, defender1);
-        System.out.println("    testNodeCoverage");
-
-        fight1.setEnvironment(Environment.Weather.rainy);
-        Attack attack = new Attack(70, "Fire");
-
-        double damage = fight1.calculateDamage(attack, attacker1, defender1);
-        System.out.println("         Damage dealt: " + damage);
-        assertEquals(damage, 42, 0.2);
-    }
-
-    // Edge coverage test case will visit every node in path
-    // All branches of code have been executed each once in BattleScenario.java
-    // The while loop creates a lot of possibilities for each branch to execute
-    @Test
-    public void testEdgeCoverage() throws Exception {
-
-        Mascotmon attacker1 = new Mascotmon(Mascotmon.Name.BULLY);
-        Mascotmon defender1 = new Mascotmon(Mascotmon.Name.RALPHIE);
-
-        BattleScenario fight1 = new BattleScenario(attacker1, defender1);
-        System.out.println("    testEdgeCoverage");
-
-        fight1.setEnvironment(Environment.Weather.sunny);
-        Attack attack = new Attack(80, "Ground");
-
-        double damage = fight1.calculateDamage(attack, attacker1, defender1);
-        System.out.println("         Damage dealt: " + damage);
-        assertEquals(damage, 15, 0.2);
-    }
 
 }
